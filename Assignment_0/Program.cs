4993515be8a91c186f465c61ba268361c047e60a
venkey <venkeyvenkatesh885@gmail.com>
2020-09-27 20:43:28 +0530

    [venkey] Refactor. Added attendance

--- Assignment_0/Program.cs
+++ Assignment_0/Program.cs
@@ -6,7 +6,17 @@ namespace Assignment_0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Emplyoyee");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
 
         }
     }
