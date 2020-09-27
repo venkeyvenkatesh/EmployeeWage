94cec06de9ecc9f127f096a138e83f99e3b05c8d
venkey <venkeyvenkatesh885@gmail.com>
2020-09-27 21:22:26 +0530

    [venkey] Refactor. Added monthly wage

--- Assignment_0/Program.cs
+++ Assignment_0/Program.cs
@@ -2,42 +2,49 @@
 
 namespace Assignment_0
 {
+    
+    
     class Program
     {
+
         // contants
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
 
         static void Main(string[] args)
         {
-            Random rand = new Random();
-
-          
-            int wage = 20;
-
             //variable
-            int type = rand.Next(0, 3);
-            int hours = 0;
-            int total_wage = 0;
+            
+            int empHrs = 0,empWage = 0, totalEmpWage = 0;
 
-            switch (type)
+            for(int day=1;day<=NUM_OF_WORKING_DAYS;day++)
             {
-                case IS_FULL_TIME:
-                    hours = 8;
-                    break;
-                case IS_PART_TIME:
-                    hours = 4;
-                    break;
-                default:
-                    hours = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                       empHrs = 4;
+                        break;
+                    default:
+                       empHrs = 0;
+                        break;
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage = totalEmpWage + empWage;
+
+                Console.WriteLine("Emp Wage for day "+ day +" is :" + empWage);
+
             }
+           
 
-
-            total_wage = hours * wage;
-
-
-            Console.Out.WriteLine(total_wage);
+            Console.Out.WriteLine("Total Emp wage : "+totalEmpWage);
         }
     }
 }
